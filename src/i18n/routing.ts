b97f777beb57020741af270acb2185c8e7ac853d
claudiolibanez <claudiolibanez@gmail.com>
2025-11-24 00:12:00 -0300

    test

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,5 +3,5 @@ import { defineRouting } from "next-intl/routing";
 export const routing = defineRouting({
   locales: ["pt-BR", "en"],
   defaultLocale: "pt-BR",
-  localePrefix: "as-needed",
+  localePrefix: "never",
 });
